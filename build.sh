4bd754339ebe5dc043490768885471a30a5d320d
tym <1772314831@qq.com>
2022-02-09 14:34:56 +0800

    chore: build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
-yarn
-
-image=tmaize/apisix-acme:1.0.2
-docker build --no-cache --rm --tag ${image} .
+image=apisix-acme:1.0.2
 
 input=$1
 
 case $input in
+  build)
+    yarn
+    docker build --no-cache --rm --tag ${image} .
+    ;;
   publish)
-    docker push ${image}
+    img_dockerhub=tmaize/${image}
+    docker tag ${image} ${img_dockerhub}
+    docker push ${img_dockerhub}
+    ;;
+  publish2)
+    img_tencent=ccr.ccs.tencentyun.com/free-guangzhou/${image}
+    docker tag ${image} ${img_tencent}
+    docker push ${img_tencent}
     ;;
   *)
-    echo ""
+    echo "no input"
     ;;
 esac
